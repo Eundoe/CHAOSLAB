e06d9e58730ce143021541e663fa07f3eaa3f260
Eundoe <eundoe92@gmail.com>
2022-12-02 23:58:48 +0900

    test001

---
+++ src/class1.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+class ThisIsWork3 extends React.Component{
+  render(){
+    return(
+      <>
+      <div>import한 Class component도 정상작동됩니다.</div>
+      <p>해당 위치는 import의 위치에 따라 달라질 수 있습니다.</p>
+      </>
+    )
+  }
+}
+
+export default ThisIsWork3
--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client"
+//import test
+import ThisIsWork3 from "./class1";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+
+class ThisIsWork2 extends React.Component{
+  render(){
+    return(
+      <>
+      <div>root.render보다 이전에 만들어진 class component</div>
+      <p>정상적으로 작동합니다.</p>
+      </>
+    )
+  }
+}
+
+
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <div>Hello world</div>
+    <ThisIsWork/>
+    <ThisIsWork2/>
+    {/* <ThisIsnotWork/> */}
+    <ThisIsWork3/>
   </React.StrictMode>
-);
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+// component initial text must be upper case 
+
+function ThisIsWork (){
+  return(
+    <>
+      <div>function component는 작동합니다</div>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </>
+  )
+}
+
+// 주석처리한 error part
+// class ThisIsnotWork extends React.Component{
+//   render(){
+//     return(
+//       <>
+//       <div>root.render보다 늦게 만들어진 class component</div>
+//       <p>작동하지 않습니다.</p>
+//       </>
+//     )
+//   }
+// }
